2f052dc1725172370f879297628d84ccd18b68e5
alumno <alumno@PC-XX>
2019-02-05 14:14:50 +0100

    2º commit

--- GitHub project/src/prueba.java
+++ GitHub project/src/prueba.java
@@ -1,9 +1,14 @@
-
 public class prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int aux=0;
+		
+		if(aux==0) {
+			System.out.println("Holi");
+		}else {
+			System.out.println("Acabóse");
+		}
 	}
 
 }
---
+++ GitHub project/src/puebaII.java
@@ -0,0 +1,9 @@
+
+public class puebaII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
