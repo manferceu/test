7d696dab50e07c43af4a5600a8e19f7025320bb1
alumno <alumno@PC-XX>
2019-02-05 14:17:05 +0100

    3º commit

--- GitHub project/src/puebaII.java
+++ GitHub project/src/puebaII.java
@@ -3,7 +3,12 @@ public class puebaII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i=0;
+		while (i==0) {
+			if (i==0) {
+				System.out.println("Bucle infinito by the way");
+			}
+		}
 	}
 
 }
